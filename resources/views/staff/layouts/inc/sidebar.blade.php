b532bb692d1a6f0b61484787f87356a419f11fb3
unknown <eulscanuel@gmail.com>
2020-12-26 14:18:23 +0800

    Final change

--- resources/views/staff/layouts/inc/sidebar.blade.php
+++ resources/views/staff/layouts/inc/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <div>
+    <div class="container">
     <a href="{{ route('staff.dashboard') }}" class="brand-link {{ Request::is('staff/dashboard') ? 'active' : ''}}">
     
     @if(count($companies) > 0)
@@ -16,7 +16,7 @@
            alt="Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-bold">{{ Auth::user()->name }}&nbspAdmin</span>
+      <span class="brand-text font-weight-bold">Branch Admin</span>
     </a>
     @endif
     </div >
