c4f4e3af3430a2537780bdcc7ba09b10f68ffc2d
unknown <eulscanuel@gmail.com>
2021-04-17 10:21:55 +0800

    Changes of Data Type

--- database/migrations/2020_11_19_021613_create_couriers_table.php
+++ database/migrations/2020_11_19_021613_create_couriers_table.php
@@ -31,7 +31,7 @@ class CreateCouriersTable extends Migration
             $table->string('recipient_pin');
             $table->string('description');
             $table->string('weight');
-            $table->string('price');
+            $table->bigInteger('price');
             $table->string('status');
             $table->timestamps();
         });
