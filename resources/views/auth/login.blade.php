6a7078cbc0ee6b98f34ee3ee292d30919d0ce82e
unknown <eulscanuel@gmail.com>
2021-04-30 09:59:56 +0800

    Admin page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <link rel="icon" href={{ asset('admin/img/logo.svg') }}>
+
+  <title>Courier and Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="{{ asset('admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+  <!-- Custom fonts -->
+  <link href="{{ asset('admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('admin/vendor/simple-line-icons/css/simple-line-icons.css') }}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
   <!-- Ionicons -->
@@ -17,12 +28,34 @@
   <link rel="stylesheet" href="{{ asset('admin/dist/css/adminlte.min.css') }}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <!-- Custom styles -->
+  <link href="{{ asset('admin/css/landing-page.min.css') }}" rel="stylesheet">
+
 </head>
-<body class="hold-transition login-page">
-<div class="login-box" style="padding-bottom: 70px">
-  <div class="login-logo">
-    <a href="#"><b>Courier Management Admin</b></a>
-  </div>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-light bg-light static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="{{ asset('admin/img/logo.svg') }}" width="30" height="30" class="d-inline-block align-top pr-2" alt="">
+        COURIER SYSTEM
+      </a>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-4">Courier Management Admin</h1>
+        </div>
+
+<div class="login-box mx-auto" style="width:27rem; height:14rem;">
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
@@ -60,7 +93,7 @@
 
         <div class="row">
           <div class="col-8">
-            <div class="icheck-primary">
+            <div class="icheck-primary" style="text-align:left">
               <input type="checkbox" id="remember" name="remember">
               <label for="remember">
                 Remember Me
@@ -80,14 +113,24 @@
     <!-- /.login-card-body -->
   </div>
 </div>
-<!-- /.login-box -->
 
-<!-- jQuery -->
+    </div>
+  </header>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- jQuery -->
 <script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('admin/dist/js/adminlte.min.js') }}"></script>
-
 </body>
+
 </html>
+
+
+
+
+
